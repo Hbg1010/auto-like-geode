c2296c604d3e809f5652f3500d9c481008d0af6b
hbg1010 <harrisong26@parkschool.net>
2025-05-11 13:16:18 -0400

    i think this will work

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,12 @@ class $modify(LikeLevelAuto, LevelInfoLayer) {
         if (!Mod::get()->getSettingValue<bool>("enable")) return;
         
         if (m_likeBtn && m_likeBtn->isEnabled()) {
-            //this works and i wish it didnt. easiest soloution though
-            // and before you ask, no it does not duplicate likes. IDK WHY
             LikeItemLayer* extraLikeLayer = LikeItemLayer::create(LikeItemType::Level, m_level->m_levelID, 0);
-            LevelInfoLayer::onLike(nullptr);
-            if (LikeItemLayer* parent = getParent()->getChildByType<LikeItemLayer*>(0)) {
-                parent->setVisible(false);
-                parent->onLike(nullptr);
-            }
-
-            #ifndef GEODE_IS_DESKTOP
+            extraLikeLayer->onLike(nullptr);
             CCSprite* disableButton = CCSprite::createWithSpriteFrameName("GJ_like2Btn2_001.png");
             m_likeBtn->setSprite(disableButton);
             m_likeBtn->m_bEnabled = false;
             incrementLikes();
-            #endif
-
-            // Lets just be safe
             CC_SAFE_DELETE(extraLikeLayer);
         }
     }
