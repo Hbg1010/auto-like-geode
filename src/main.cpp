c352a614269b4ee2720bce8f3cf7cac978537bb7
hbg1010 <harrisong26@parkschool.net>
2025-05-11 01:43:30 -0400

    cross platform fix?

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,16 @@ class $modify(LikeLevelAuto, LevelInfoLayer) {
             LikeItemLayer* extraLikeLayer = LikeItemLayer::create(LikeItemType::Level, m_level->m_levelID, 0);
             LevelInfoLayer::onLike(nullptr);
             if (auto parent = getParent()->getChildByType<LikeItemLayer*>(0)) {
+                parent->setVisible(false);
                 parent->onLike(nullptr);
             }
+
+            #ifndef GEODE_IS_DESKTOP
+            CCSprite* disableButton = CCSprite::createWithSpriteFrameName("GJ_like2Btn2_001.png");
+            m_likeBtn->setSprite(disableButton);
+            m_likeBtn->m_bEnabled = false;
+            incrementLikes();
+            #endif
 
             // Lets just be safe
             CC_SAFE_DELETE(extraLikeLayer);
